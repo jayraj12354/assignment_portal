532dde6cdce8929bdd45103902519615dd6fe254
jayraj <jairaj12354@gmail.com>
2026-09-23 09:46:29 +0530

    post route added

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
-const { Pool } = require('pg');
-require('dotenv').config();
+const path = require('node:path');
+const FilePath = path.join(__dirname, "./db.js")
+const pool = require(FilePath);
+const express = require('express');
 
-const pool = new Pool(
-    {
-    user:process.env.DB_USER,
-    host:process.env.DB_HOST,
-    port:process.env.DB_PORT,
-    password:process.env.DB_PASSWORD,
-    database:process.env.DB_DATABASE
+const app = express();;
+app.use(express.json());
 
+app.post('/assignments', async(req, res)=>{
+    try{
+        const {title, deadline}=req.body;
+        let result = await pool.query(`
+            INSERT INTO assignments(title, deadline)
+            values($1, $2)
+            returning *;
+            `, [title, deadline])
+        res.status(201).json(result.rows[0])
+    }catch(err){
+        console.log(err)
+        res.status(500).json({err:"Server Failed"});
+    }
+})
+
+app.listen(3000,() => {
+    console.log('Server is running')
 });
-module.exports = pool;
